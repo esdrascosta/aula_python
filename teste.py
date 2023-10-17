8328fd2286af98007dc37cf7a2ebc9bad7aac2e6
Mario <itmemario@mail.com>
2023-10-17 20:58:47 -0300

    refactor: change the function

--- teste.py
+++ teste.py
@@ -1,4 +1,9 @@
-
+def sum_if(data, condition):
+    return sum([i for i in data if condition(i)])
 
 if __name__ == '__main__':
-    print('Olá')
+    
+    data = list(range(20))
+    result = sum_if(data, lambda x: x > 10)
+
+    print(result)
